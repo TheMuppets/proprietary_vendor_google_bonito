d7de2740fc548cbaf6b6f67aa3105a7f92fe062a
Michael Bestas <mkbestas@lineageos.org>
2025-01-07 01:59:44 +0200

    bonito: Remove OMX blobs
    Change-Id: Iddf23261a66267fa9062fcc5f65ec5ad31f3a3ab

--- bonito-vendor.mk
+++ bonito-vendor.mk
@@ -1017,21 +1017,12 @@ PRODUCT_PACKAGES += \
     sound_trigger.primary.sdm710 \
     vendor.qti.hardware.qteeconnector@1.0-impl \
     vulkan.adreno \
-    libAlacSwDec \
-    libApeSwDec \
     libC2D2 \
     libCB \
-    libFlacSwDec \
     libGPQTEEC_vendor \
     libGPTEE_vendor \
     libGPreqcancel \
     libGPreqcancel_svc \
-    libMpeg4SwEncoder \
-    libOmxAlacDecSw \
-    libOmxAmrwbplusDec \
-    libOmxApeDecSw \
-    libOmxEvrcDec \
-    libOmxQcelp13Dec \
     libOpenCL-pixel \
     libOpenCL \
     libQSEEComAPI \
@@ -1156,11 +1147,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.wigig.netperftuner@1.0 \
     vendor.qti.power.pasrmanager@1.0 \
     vendor.qti.voiceprint@1.0 \
-    libOmxDsdDec \
     libadm \
     libadpcmdec \
     libcppf \
-    libdsd2pcm \
     libfastcrc \
     libfastcvdsp_stub \
     libfastcvopt \
